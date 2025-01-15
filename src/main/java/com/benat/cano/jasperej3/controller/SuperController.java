1ab1faabd934539a995b0c48e22edfb1ed1cd4a8
Becanar <benatortubitarte@gmail.com>
2025-01-15 08:36:09 +0100

    Añado fxml y esqueleto controller

---
+++ src/main/java/com/benat/cano/jasperej3/controller/SuperController.java
@@ -0,0 +1,41 @@
+package com.benat.cano.jasperej3.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SuperController {
+
+    @FXML
+    private Button btGraficos;
+
+    @FXML
+    private Button btProductos;
+
+    @FXML
+    private Button btSecciones;
+
+    @FXML
+    private Button btTabProductos;
+
+    @FXML
+    void infoGraficos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void infoProductos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void infoSecciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void infoTabProductos(ActionEvent event) {
+
+    }
+
+}
